fe0d0f46e6c296dacff05c1db80d3a8e60fb14e4
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2019-11-19 11:23:12 +0700

    benerin query pencarian surat

--- laravel/app/Http/Controllers/SuratController.php
+++ laravel/app/Http/Controllers/SuratController.php
@@ -9,28 +9,36 @@ use function GuzzleHttp\json_encode;
 
 class SuratController extends Controller
 {
-    public function getSurat(Request $request){
-        
-        
+    public function getSurat(Request $request)
+    {
         $surat  = Surat::latest('id')->where('jenis', 'keluar');
-        if ($request->has('q')) {
-            $q = $request->input('q');
-            $surat = $surat->where('nomer', 'LIKE', "%$q%")
-                ->orWhere('tanggal', 'LIKE', "%$q%")
-                ->orWhere('perihal', 'LIKE', "%$q%")
-                ->orWhere('dari', 'LIKE', "%$q%")
-                ->orWhere('type', 'LIKE', "%$q%")
-                ->orWhere('jenis', 'LIKE', "%$q%")
-                ->orWhere('nik', 'LIKE', "%$q%");
+        if ($request->input('q') != "" || $request->input('tahun') != "") {
+            $q      = $request->input('q');
+            $tahun  = $request->input('tahun');
+
+            if ($q != "") {
+                $surat->where(function ($query) use ($q) {
+                    $query->where('nomer', 'LIKE', "%$q%")
+                        ->orWhere('tanggal', 'LIKE', "%$q%")
+                        ->orWhere('perihal', 'LIKE', "%$q%")
+                        ->orWhere('dari', 'LIKE', "%$q%")
+                        ->orWhere('type', 'LIKE', "%$q%")
+                        ->orWhere('jenis', 'LIKE', "%$q%")
+                        ->orWhere('nik', 'LIKE', "%$q%");
+                });
+            }
+
+            if ($tahun != "") {
+                $surat->where(function ($query2) use ($tahun) {
+                    $query2->whereYear("tanggal", "=", $tahun);
+                });
+            }
         }
-        if ($request->has('tahun')) {
-            $t = $request->input('tahun');            
-            $surat = $surat->whereYear('tanggal', '=', "$t");
-        }
+
         $surat = $surat->paginate(10);
 
         $jenis  = Surat::distinct()->get(["type"]);
-        $tahun  = Surat::select(DB::raw('YEAR(created_at) as tahun'))->distinct()->get();                
+        $tahun  = Surat::select(DB::raw('YEAR(created_at) as tahun'))->distinct()->get();
         $type   = "Keluar";
         // die(json_encode($surat));
         return view('kesekretariatan.surat.index2', compact('surat', 'jenis', 'tahun', 'type'));
